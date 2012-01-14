381ae311db7f53166bdb133becf9ce12b64cb95c
Iuri de Silvio <iurisilvio@gmail.com>
2012-01-14 17:50:08 -0200

    Add a release.py script to create a release folder.

---
+++ release.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import os
+import shutil
+import zipfile
+
+FILES = ["manifest.json",
+         "fb.js",
+         "options.js",
+         "background.html",
+         "options.html"]
+
+RELEASE_DIR = "release"
+
+if os.path.isdir(RELEASE_DIR):
+    raise Exception("Release directory already exists.")
+
+os.mkdir(RELEASE_DIR)
+for f in FILES:
+    shutil.copy2(f, os.path.join(RELEASE_DIR, f))
