b2bead8b65c9c88fce4e6db8505825057ffca1f8
Iuri de Silvio <iurisilvio@gmail.com>
2012-07-28 19:06:34 -0300

    Update release.py script to generate the zip file to publish.

--- release.py
+++ release.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python
 import os
-import shutil
+import shutil, errno
 import zipfile
+
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as ex:
+        if ex.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else:
+            raise
+
+def rmanything(src):
+    try:
+        try:
+            shutil.rmtree(src)
+        except OSError as ex:
+            if ex.errno == errno.ENOTDIR:
+                os.remove(src)
+    except OSError as ex:
+        if ex.errno != errno.ENOENT:
+            raise
+
+def zipdir(src, dst):
+    zip = zipfile.ZipFile(dst, 'w', zipfile.ZIP_DEFLATED)
+    rootlen = len(src) + 1
+    for base, dirs, files in os.walk(src):
+       for file in files:
+          fn = os.path.join(base, file)
+          zip.write(fn, fn[rootlen:])
 
 FILES = ["manifest.json",
          "fb.js",
          "options.js",
          "options_page.js",
-         "background.js",
+         "background.html",
          "jquery.js",
          "options.html",
-         "icon-16.png"]
+         "icon-16.png",
+         "_locales"]
 
 RELEASE_DIR = "release"
+RELEASE_FILE = "release.zip"
 
-if os.path.isdir(RELEASE_DIR):
-    raise Exception("Release directory already exists.")
-
+rmanything(RELEASE_DIR)
+rmanything(RELEASE_FILE)
 os.mkdir(RELEASE_DIR)
+
 for f in FILES:
-    shutil.copy2(f, os.path.join(RELEASE_DIR, f))
+    copyanything(f, os.path.join(RELEASE_DIR, f))
+
+zipdir(RELEASE_DIR, RELEASE_FILE)
